93607909b42aca700b9b514b8f5c91dc96caa66a
Rjay Lane <rjlane@andrew.cmu.edu>
2021-02-08 10:25:31 -0500

    final commit

--- app/models/child.rb
+++ app/models/child.rb
@@ -10,4 +10,8 @@ class Child < ApplicationRecord
   def name
     first_name + " " + last_name
   end
+
+  def points_earned
+    self.chores.done.inject(0){|sum,chore| sum += chore.task.points}
+  end 
 end
--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -32,6 +32,10 @@ class ChildTest < ActiveSupport::TestCase
     should "have a scope to select only active children" do
       assert_equal ["Alex", "Mark"], Child.active.alphabetical.map{|c| c.first_name}
     end
+
+    should "have a method to show points earned" do
+      assert_equal 4, @alex.points_earned
+    end
   end
 
 end

606feaa23441e50108a81a2d24da468ec6405ec5
Rjay Lane <rjlane@andrew.cmu.edu>
2021-02-08 10:02:44 -0500

    child model complete

--- app/models/child.rb
+++ app/models/child.rb
@@ -3,4 +3,11 @@ class Child < ApplicationRecord
   has_many :tasks, through: :chores
 
   validates_presence_of :first_name, :last_name
+
+  scope :alphabetical, -> { order(:last_name, :first_name) }
+  scope :active, -> { where(active: true) }
+
+  def name
+    first_name + " " + last_name
+  end
 end
